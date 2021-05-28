68de1c083077ca6ec5e166bd21d2b4e62fa6c931
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-05-28 11:52:44 -0400

    Refactor, move terms to top level.

--- js/context-v1.js
+++ js/context-v1.js
@@ -14,60 +14,54 @@ module.exports = {
     image: 'https://schema.org/image',
     name: 'https://schema.org/name',
 
+    // Used in OverAgeTokenCredential and AgeVerificationCredential
+    overAge: {
+      '@id': 'https://w3id.org/age#overAge',
+      '@type': 'http://www.w3.org/2001/XMLSchema#positiveInteger'
+    },
+
+    concealedIdToken: {
+      '@id': 'https://w3id.org/cit#concealedIdToken',
+      '@type': 'https://w3id.org/security#multibase'
+    },
+
+    anchoredResource: 'https://w3id.org/security#anchoredResource',
+    contentHash: 'https://w3id.org/security#contentHash',
+
     // Used for the PersonalPhotoCredential
     PersonalPhotoCredential:
       'https://convenience.org/vocab#PersonalPhotoCredential',
 
     // Used for the OverAgeTokenCredential
-    OverAgeTokenCredential: {
-      '@id': 'https://w3id.org/age#OverAgeTokenCredential',
-      '@context': {
-        '@protected': true,
-        overAge: {
-          '@id': 'https://w3id.org/age#overAge',
-          '@type': 'http://www.w3.org/2001/XMLSchema#positiveInteger'
-        },
-        concealedIdToken: {
-          '@id': 'https://w3id.org/cit#concealedIdToken',
-          '@type': 'https://w3id.org/security#multibase'
-        }
-      }
-    },
+    OverAgeTokenCredential: 'https://w3id.org/age#OverAgeTokenCredential',
 
     AgeVerificationCredential: {
       '@id': 'https://w3id.org/age#AgeVerificationCredential',
       '@context': {
         '@protected': true,
-        overAge: {
-          '@id': 'https://w3id.org/age#overAge',
-          '@type': 'http://www.w3.org/2001/XMLSchema#positiveInteger'
-        },
-        anchoredResource: 'https://w3id.org/security#anchoredResource',
-        contentHash: 'https://w3id.org/security#contentHash',
 
         // refreshService section of the VC
-        // eslint-disable-next-line max-len
-        VerifiableCredentialRefreshService2021: 'https://w3id.org/vc-refresh-service#VerifiableCredentialRefreshService2021',
+        VerifiableCredentialRefreshService2021: {
+          // eslint-disable-next-line max-len
+          '@id': 'https://w3id.org/vc-refresh-service#VerifiableCredentialRefreshService2021',
+          '@context': {
+            '@protected': true,
+            url: {
+              '@id': 'https://w3id.org/vc-refresh-service#url',
+              '@type': '@id'
+            },
+            refreshToken: {
+              '@id': 'https://w3id.org/vc-refresh-service#refreshToken',
+              '@type': 'https://w3id.org/security#multibase'
+            }
+          }
+        },
         refreshService:
           'https://w3id.org/vc-refresh-service#refreshService',
-        url: {
-          '@id': 'https://w3id.org/vc-refresh-service#url',
-          '@type': '@id'
-        },
-        refreshToken: {
-          '@id': 'https://w3id.org/vc-refresh-service#refreshToken',
-          '@type': 'https://w3id.org/security#multibase'
-        }
       }
     }
   },
 
-  AgeVerificationContainerCredential: {
-    '@id': 'https://w3id.org/age#AgeVerificationContainerCredential',
-    '@context': {
-      '@protected': true,
-      anchoredResource: 'https://w3id.org/security#anchoredResource',
-      contentHash: 'https://w3id.org/security#contentHash'
-    }
-  }
+  AgeVerificationContainerCredential:
+    'https://w3id.org/age#AgeVerificationContainerCredential'
 };
